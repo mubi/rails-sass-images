e7df580b33fd469d7e1459873a82304fc0b2e37d
James Adam <james@lazyatom.com>
2019-10-17 17:37:38 +0100

    Make compatible with Sprockets 4.0.0
    The `Sprockets::SassFunctions` module is gone now, and replaced by two
    others, corresponding to the different SASS engines that might be used.
    
    Also, `Sprockets::Index` doesn't exist anymore, but instead we might
    be returned a `Sprockets::CachedEnvironment`.
    
    Finally, `Sprockets::Asset#pathname` is gone, replaced with
    `#filename`, but this returns a string whereas other parts of this gem
    assume that they can call `read` on it, so we need to re-wrap it in a
    `Pathname` instance.

--- lib/rails-sass-images/sass.rb
+++ lib/rails-sass-images/sass.rb
@@ -4,7 +4,17 @@ dir = Pathname(__FILE__).dirname.join('sass')
 Dir.glob(dir.join('*.rb').to_s) { |ext| require ext }
 
 module Sprockets
-  module SassFunctions
-    include RailsSassImages::Sass
+  class SassProcessor
+    module Functions
+      include RailsSassImages::Sass
+    end
+  end
+end
+
+module Sprockets
+  class SasscProcessor
+    module Functions
+      include RailsSassImages::Sass
+    end
   end
 end
