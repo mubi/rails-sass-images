2e724f445a071fd2f601e76c5bd0b7e86305914d
James Adam <james@lazyatom.com>
2019-10-22 13:36:38 +0100

    ActionController::TestResponse#success? is gone
    It's now replaced with `#successful?`

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -8,7 +8,7 @@ describe CssController, type: :controller do
 
   it "inlines assets" do
     get :test, params: { file: 'inline' }
-    expect(response).to be_success
+    expect(response).to be_successful
     expect(response.body).to eq ".icon{background:#{INLINE}}\n"
   end
 
@@ -20,19 +20,19 @@ describe CssController, type: :controller do
 
   it "gets image size" do
     get :test, params: { file: 'size' }
-    expect(response).to be_success
+    expect(response).to be_successful
     expect(response.body).to eq ".icon{width:4px;height:6px}\n"
   end
 
   it "gets image size by mixin" do
     get :test, params: { file: 'image-size' }
-    expect(response).to be_success
+    expect(response).to be_successful
     expect(response.body).to eq ".icon{width:4px;height:6px}\n"
   end
 
   it "has hidpi-background mixin" do
     get :test, params: { file: 'hidpi-background' }
-    expect(response).to be_success
+    expect(response).to be_successful
     expect(response.body).to include ".icon{" +
       "background-image:url(/assets/monolith"
     expect(response.body).to include "background-size:2px 3px}"
@@ -40,7 +40,7 @@ describe CssController, type: :controller do
 
   it "has hidpi-image mixin" do
     get :test, params: { file: 'hidpi-image' }
-    expect(response).to be_success
+    expect(response).to be_successful
     expect(response.body).to include ".icon{" +
       "width:2px;height:3px;" +
       "background-image:url(/assets/monolith"
@@ -50,7 +50,7 @@ describe CssController, type: :controller do
 
   it "has hidpi-inline mixin" do
     get :test, params: { file: 'hidpi-inline' }
-    expect(response).to be_success
+    expect(response).to be_successful
     expect(response.body).to eq ".icon{" +
       "width:2px;height:3px;" +
       "background:#{INLINE} no-repeat;" +
